4330ecccbae7fa6a87b282f5abf39f0b8330f418
Marius Ionut Toader <marius.t@gtmarketing.ro>
2023-10-27 11:33:17 +0300

    +Test new code - remove qodana - v1

--- CleanCode_Consistency.php
+++ CleanCode_Consistency.php
@@ -8,6 +8,8 @@ class CleanCode_Consistency
     #Clean code attribute - Consistency; Software Quality - Maintability; Severity - Low
     public $name_v2 = NULL;  // instance variable - v2
 
+    public $name_v3 = NULL;  // instance variable - v3
+
 
 
 }
