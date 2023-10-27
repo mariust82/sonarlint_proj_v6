34a095aa24e34c4930c569263e0829e98a3ea866
Marius Ionut Toader <marius.t@gtmarketing.ro>
2023-10-27 10:24:02 +0300

    +Test new code - v2

--- CodeSmell.php
+++ CodeSmell.php
@@ -15,4 +15,18 @@ class CodeSmell
                 break;
         }
     }
+
+    public function HighSeverity_v2($param_v2){
+        switch ($param_v2) {
+            case 0:
+                doSomething_v2();
+                break;
+            default: // Noncompliant: default clause should be the first or last one
+                error();
+                break;
+            case 1:
+                doSomethingElse_v2();
+                break;
+        }
+    }
 }
