8114adb56a69050ec0d048587f9fae3bb37452fc
Marius Ionut Toader <marius.t@gtmarketing.ro>
2023-10-27 11:22:27 +0300

    +Test new code - CleanCode_Consistency.php - v1

--- CleanCode_Consistency.php
+++ CleanCode_Consistency.php
@@ -5,6 +5,9 @@ class CleanCode_Consistency
     #Clean code attribute - Consistency; Software Quality - Maintability; Severity - Low
     public $name = NULL;  // instance variable
 
+    #Clean code attribute - Consistency; Software Quality - Maintability; Severity - Low
+    public $name_v2 = NULL;  // instance variable - v2
+
 
 
 }
